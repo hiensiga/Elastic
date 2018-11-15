e1ba5a768e00ec496d221675502289e0d5251165
Doan Phuc Hien <hien.dp@gnt.com.vn>
2018-11-15 17:51:30 +0700

    init project

---
+++ Elastic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Elastic
+//
+//  Created by Doan Phuc Hien on 11/15/18.
+//  Copyright © 2018 HSG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
