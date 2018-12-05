91ca1b357287dff8eaadae23b502e8cd2f4f43c7
Doan Phuc Hien <hien.dp@gnt.com.vn>
2018-12-05 18:12:20 +0700

    #102 structure app

---
+++ Elastic/ViewControllers/CompanyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CompanyViewController.swift
+//  Elastic
+//
+//  Created by Doan Phuc Hien on 12/5/18.
+//  Copyright © 2018 HSG. All rights reserved.
+//
+
+import UIKit
+
+class CompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
